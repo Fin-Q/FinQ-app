f8a65852b68bf9816fdc6e4e9fc82784c42e9b50
Minter <adf5858@naver.com>
2026-08-22 21:10:53 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/com/swyp/FinQ/FinQApplication.java
@@ -0,0 +1,13 @@
+package com.swyp.FinQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinQApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swyp/FinQ/FinQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swyp.FinQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinQApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
