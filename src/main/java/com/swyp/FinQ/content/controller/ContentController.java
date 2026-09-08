967a2c3dfae0966338ea4fa1bbd8bc5b63386b49
yezanee <rbfrk02@naver.com>
2026-09-08 19:16:40 +0900

    fix: ContentController 인증 연동 및 에러코드 정합성 수정

--- src/main/java/com/swyp/FinQ/content/controller/ContentController.java
+++ src/main/java/com/swyp/FinQ/content/controller/ContentController.java
@@ -12,6 +12,8 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,22 +25,22 @@ public class ContentController {
 
     private final ContentQueryService contentQueryService;
 
-    // TODO: 인증 구현 후 토큰에서 userId 추출로 변경
-    private static final Long TEMP_USER_ID = 1L;
-
     @Operation(summary = "지식맵 조회", description = "전체 카테고리 목록과 사용자별 진행률을 조회합니다.")
     @GetMapping("/knowledge-map")
-    public ResponseEntity<SuccessResponse<KnowledgeMapResponse>> getKnowledgeMap() {
-        KnowledgeMapResponse response = contentQueryService.getKnowledgeMap(TEMP_USER_ID);
+    public ResponseEntity<SuccessResponse<KnowledgeMapResponse>> getKnowledgeMap(
+            @AuthenticationPrincipal Jwt jwt
+    ) {
+        KnowledgeMapResponse response = contentQueryService.getKnowledgeMap(Long.valueOf(jwt.getSubject()));
         return SuccessResponse.of(ContentSuccessCode.KNOWLEDGE_MAP_RETRIEVED, response);
     }
 
     @Operation(summary = "카테고리 상세 조회", description = "카테고리별 콘텐츠 목록과 완료 상태를 조회합니다.")
     @GetMapping("/categories/{categoryCode}")
     public ResponseEntity<SuccessResponse<CategoryDetailResponse>> getCategoryDetail(
+            @AuthenticationPrincipal Jwt jwt,
             @Parameter(description = "카테고리 코드") @PathVariable CategoryCode categoryCode
     ) {
-        CategoryDetailResponse response = contentQueryService.getCategoryDetail(categoryCode, TEMP_USER_ID);
+        CategoryDetailResponse response = contentQueryService.getCategoryDetail(categoryCode, Long.valueOf(jwt.getSubject()));
         return SuccessResponse.of(ContentSuccessCode.CATEGORY_DETAIL_RETRIEVED, response);
     }
 
--- src/main/java/com/swyp/FinQ/learning/exception/LearningErrorCode.java
+++ src/main/java/com/swyp/FinQ/learning/exception/LearningErrorCode.java
@@ -10,7 +10,7 @@ public enum LearningErrorCode implements ErrorCode {
 
     QUESTION_NOT_FOUND(HttpStatus.NOT_FOUND, "QUESTION_NOT_FOUND", "존재하지 않는 문제입니다."),
     QUIZ_NOT_FOUND(HttpStatus.NOT_FOUND, "QUIZ_NOT_FOUND", "존재하지 않는 심화퀴즈입니다."),
-    INVALID_OPTION(HttpStatus.BAD_REQUEST, "QUIZ_INVALID_ANSWER", "유효하지 않은 보기입니다."),
+    INVALID_OPTION(HttpStatus.BAD_REQUEST, "INVALID_OPTION", "유효하지 않은 보기입니다."),
     QUESTION_CONTENT_MISMATCH(HttpStatus.BAD_REQUEST, "QUESTION_CONTENT_MISMATCH", "해당 콘텐츠에 속하지 않는 문제입니다."),
     QUIZ_CATEGORY_MISMATCH(HttpStatus.BAD_REQUEST, "QUIZ_CATEGORY_MISMATCH", "해당 카테고리에 속하지 않는 심화퀴즈입니다.");
 
